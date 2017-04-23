b85170302bf2b49a5186028647842215f0d5e723
ManWithShotgun <u3n3ter7@mail.ru>
2017-04-23 22:38:35 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+Ant build.xml use log4j for log build process.
+
+Run: 0) mvn package
+     1) ant-run-default.bat (run with default target)
+
---
+++ ant-run-default.bat
@@ -0,0 +1,1 @@
+call ant -buildfile ant\build.xml -listener org.apache.tools.ant.listener.Log4jListener -lib target\ant-script-1.0-SNAPSHOT-jar-with-dependencies.jar
